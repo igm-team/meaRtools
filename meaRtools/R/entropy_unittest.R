f1717ea4e7b883d427f09da81b07753362c4aa16
QuanliWang <quanliwang20@hotmail.com>
2017-07-05 13:51:00 -0400

    remove zzz.R and lintr check/fix for entropy_unittest.R

--- meaRtools/R/entropy_unittest.R
+++ meaRtools/R/entropy_unittest.R
@@ -1,69 +1,72 @@
 # unit tests for MEA functions
-# stopifnot(test.Entropy.funcs()==T)
-# stopifnot(test.Entropy.funcs.MEA()==T)
+# stopifnot(test_entropy_funcs()==T)
+# stopifnot(test_entropy_funcs.mea()==T)
 
-##################################################################### 
-# 
+#####################################################################
+#
 # unit testing functions
-# 
-##################################################################### 
+#
+#####################################################################
 
-.test.Entropy.funcs <- function() {
+.test_entropy_funcs <- function() {
   # general tests for entropy calculations on a perfectly uniform dist
-  t.span = c(0, 1600)
-  spikeset.max.entropy <- c(100, 300, 500, 700, 900, 1100, 1300, 1500)
-  testbin.starts <- c(0, 200, 550, 900, 1200, 1450)
-  testbin.ends <- c(200, 550, 900, 1200, 1450, 1600)
-  max.ent.pdist <- .pdist.electrode.spikes(spikeset.max.entropy,
-    t.span[1], t.span[2])
-  stopifnot(max(max.ent.pdist) == min(max.ent.pdist) & max(max.ent.pdist) == 0.125)
-  ent <- .entropy(max.ent.pdist)
+  t_span <- c(0, 1600)
+  spikeset_max_entropy <- c(100, 300, 500, 700, 900, 1100, 1300, 1500)
+  testbin_starts <- c(0, 200, 550, 900, 1200, 1450)
+  testbin_ends <- c(200, 550, 900, 1200, 1450, 1600)
+  max_ent_pdist <- .pdist_electrode_spikes(spikeset_max_entropy,
+    t_span[1], t_span[2])
+  stopifnot(max(max_ent_pdist) == min(max_ent_pdist) &
+              max(max_ent_pdist) == 0.125)
+  ent <- .entropy(max_ent_pdist)
   stopifnot(ent == 3)
-  ent.norm <- .entropy(max.ent.pdist, normalized.uniform = T)
-  stopifnot(ent.norm == 1)
-  stopifnot(.KL.divergence(max.ent.pdist, max.ent.pdist) == 0)
-  stopifnot(.mutual.information(max.ent.pdist, max.ent.pdist) == 0)
+  ent_norm <- .entropy(max_ent_pdist, normalized_uniform = T)
+  stopifnot(ent_norm == 1)
+  stopifnot(.kl_divergence(max_ent_pdist, max_ent_pdist) == 0)
+  stopifnot(.mutual_information(max_ent_pdist, max_ent_pdist) == 0)
 
   # tests for entropy calcs on a non-uniform distribution
-  testbin.pdist <- .pdist.electrode.spikes(spikeset.max.entropy, t.span[1],
-    t.span[2], bin.starts = testbin.starts,
-    bin.ends = testbin.ends)
-  stopifnot(testbin.pdist == c(0.125, 0.250, 0.250, 0.125, 0.125, 0.125))
-  testbin.pdist.uniform <- c(1, 1, 1, 1, 1, 1)
-  stopifnot(.entropy(testbin.pdist) == 2.5)
-  stopifnot(.KL.divergence(testbin.pdist, testbin.pdist.uniform) == 0.084962500721156186678)
-  stopifnot(.mutual.information(testbin.pdist, testbin.pdist.uniform) == 0.01031810090963993301)
+  testbin_pdist <- .pdist_electrode_spikes(spikeset_max_entropy, t_span[1],
+    t_span[2], bin_starts = testbin_starts,
+    bin_ends = testbin_ends)
+  stopifnot(testbin_pdist == c(0.125, 0.250, 0.250, 0.125, 0.125, 0.125))
+  testbin_pdist_uniform <- c(1, 1, 1, 1, 1, 1)
+  stopifnot(.entropy(testbin_pdist) == 2.5)
+  stopifnot(.kl_divergence(testbin_pdist, testbin_pdist_uniform) ==
+              0.084962500721156186678)
+  stopifnot(.mutual_information(testbin_pdist, testbin_pdist_uniform) ==
+              0.01031810090963993301)
   return(T)
 }
 
-.test.Entropy.funcs.MEA <- function(test.elec="F8_43") {
-  # create synthetic spikes, burst intervals, t.0, t.end
-  test.spikes <- c(2, 3, 4, 7, 8, 9)
-  test.burst.starts <- c(2, 7)
-  test.burst.ends <- c(4, 9)
-  t.0 <- 1
-  t.end <- 10
-  # test ability to get bin.set vector, bin sizes, p(bins)
-  bin.set <- .get.bin.fullset(test.burst.starts,
-    test.burst.ends, t.0, t.end)
-  stopifnot(bin.set == c(1, 2, 4, 7, 9, 10))
-  bin.sizes <- .get.bin.sizes(bin.set)
-  stopifnot(bin.sizes == c(1, 2, 3, 2, 1))
-  pdist.bins <- .p.bins(bin.sizes)
-  stopifnot(pdist.bins == c(1 / 9, 2 / 9, 3 / 9, 2 / 9, 1 / 9))
+.test_entropy_funcs_mea <- function(test_elec="F8_43") {
+  # create synthetic spikes, burst intervals, t_0, t_end
+  test_spikes <- c(2, 3, 4, 7, 8, 9)
+  test_burst_starts <- c(2, 7)
+  test_burst_ends <- c(4, 9)
+  t_0 <- 1
+  t_end <- 10
+  # test ability to get bin_set vector, bin sizes, p(bins)
+  bin_set <- .get_bin_fullset(test_burst_starts,
+    test_burst_ends, t_0, t_end)
+  stopifnot(bin_set == c(1, 2, 4, 7, 9, 10))
+  bin_sizes <- .get_bin_sizes(bin_set)
+  stopifnot(bin_sizes == c(1, 2, 3, 2, 1))
+  pdist_bins <- .p_bins(bin_sizes)
+  stopifnot(pdist_bins == c(1 / 9, 2 / 9, 3 / 9, 2 / 9, 1 / 9))
   # test ability to get pdist of spikes in provided binstart/ends
-  pdist.spikes <- .pdist.electrode.spikes(test.spikes, t.0, t.end,
-    bin.starts = test.burst.starts,
-    bin.ends = test.burst.ends)
-  stopifnot(pdist.spikes == c(1 / 6, 2 / 6, 1 / 6, 2 / 6, 0))
+  pdist_spikes <- .pdist_electrode_spikes(test_spikes, t_0, t_end,
+    bin_starts = test_burst_starts,
+    bin_ends = test_burst_ends)
+  stopifnot(pdist_spikes == c(1 / 6, 2 / 6, 1 / 6, 2 / 6, 0))
 
   # test functions on burst-only interval subsets
-  bin.set.burst <- unique(sort(c(test.burst.starts,
-    test.burst.ends)))
-  bin.sizes.burst <- .get.bin.sizes(bin.set.burst, pairs.only = T)
-  stopifnot(bin.sizes.burst == c(2, 2))
-  pdist.bins.burst <- .p.bins(bin.sizes.burst)
-  stopifnot(pdist.bins.burst == c(0.5, 0.5))
+  bin_set_burst <- unique(sort(c(test_burst_starts,
+    test_burst_ends)))
+  bin_sizes_burst <- .get_bin_sizes(bin_set_burst, pairs_only = T)
+  stopifnot(bin_sizes_burst == c(2, 2))
+  pdist_bins_burst <- .p_bins(bin_sizes_burst)
+  stopifnot(pdist_bins_burst == c(0.5, 0.5))
 
   return(T)
 }
