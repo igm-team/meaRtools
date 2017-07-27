8711f2b1a5b33d164edd6b5905343f1277886213
Stephen J. Eglen <sje30@cam.ac.uk>
2017-07-27 09:58:26 +0100

    Note changes post merge

--- meaRtools/inst/sje1.R
+++ meaRtools/inst/sje1.R
@@ -17,10 +17,15 @@ data.file <- system.file("examples", "TC89_DIV15_A.nexTimestamps",
                               package = "sjemea")
 s <- sanger.read.spikes( data.file, beg=400, end=700)
 ns1 <- sjemea::compute.ns(s, ns.T=0.003, ns.N=10,sur=100)
-ns2 <- .compute.ns(s, ns.T=0.003, ns.N=10,sur=100)
-plot(ns2)
-all.equal(ns1, ns2)
+ns2 <- .compute_ns(s, ns_t=0.003, ns_n=10,sur=100)
+plot(ns1)
+plot_network_spikes(ns2) ## Note problem: this should  be just plot(ns2)
+colnames(ns1$measures) <- NULL
+colnames(ns2$measures) <- NULL
+all.equal(ns1, ns2, check.names = FALSE)
 
+## TODO plot_network_spikes is the wrong function name, it should be plot.ns() as this is the
+## S3 plot() class for the "ns" type.
 
 
 ## check STTC
@@ -30,6 +35,7 @@ t2 <- sttc_allspikes1(s$spikes, dt=getOption("sjemea.corr.dt"),
                                            s$rec.time[1], s$rec.time[2])
 all.equal(t1, t2)
 
+## NB: I need to check how to easily convert rec.time to rec_time everywhere.
 
 ## check correlation index
 
@@ -38,7 +44,7 @@ all.equal(t1, t2)
 ## check firing rate
 
 r1 = sjemea::make.spikes.to.frate(s$spikes)
-r2 = .make.spikes.to.frate(s$spikes)
+r2 = .make_spikes_to_frate(s$spikes)
 all.equal(r1,r2)
 
 # see issue about the inequality changing in the above function.
