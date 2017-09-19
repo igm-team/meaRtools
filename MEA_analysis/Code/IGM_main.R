a1241837457018fda03a706b81e53fa1d860130b
saharg <sg3261@cumc.columbia.edu>
2017-09-19 16:07:11 -0400

    Updating STTC into spike_summary_by_well

--- MEA_analysis/Code/IGM_main.R
+++ MEA_analysis/Code/IGM_main.R
@@ -89,10 +89,11 @@ if (length(s) > 0) {
   for (i in 1:length(s)){
     basename <- strsplit(basename(s[[i]]$file), "[.]")[[1]][1]
     cat("\n")
-    cat(paste("Calculating mutual information for ", basename, "...\n"))
+    cat(paste("Calculating STTC and mutual information for ", basename, "...\n"))
     ent_mi_data <- calculate_entropy_and_mi(s[[i]], s[[i]]$treatment, mult_factor=1.5, bin_size=0.1)
     s[[i]]$mutual_inf <- ent_mi_data[["data_dists"]][["MI"]]
     s[[i]]$entropy <- ent_mi_data [["data_dists"]][["ENT"]]
+    s[[i]]$mean_sttc <- compute_mean_sttc_by_well(s[[i]])
   }
 }
 
