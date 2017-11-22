999579ee9f52d6e928372fcd736c91397a352d05
saharg <sg3261@cumc.columbia.edu>
2017-11-21 22:59:37 -0500

    name change to fit short variable convention

--- MEA_analysis/Code/parameters.R
+++ MEA_analysis/Code/parameters.R
@@ -32,7 +32,7 @@ perm_n =100,
 elec_min_rate=0.016,
 elec_max_rate=1000,
 well_min_rate=4,
-well_filter_maximum_div_inactive_ratio=0.5,
+well_max_div_inactive_ratio=0.5,
 
 mi_par=list(beg_isi =0.1,
 end_isi =0.25,
