38836253bcfd77f5992e265ccf0b5b870c948813
Stephen J. Eglen <sje30@cam.ac.uk>
2018-06-14 12:25:01 +0100

    Test case code

--- sje_text.R
+++ sje_text.R
@@ -1,38 +1,20 @@
-######################################################################
-s = read_spikelist_text("~/proj/2018/maxwell/test1/times.csv",
-                        "~/proj/2018/maxwell/test1/pos.csv")
+require(devtools)
+load_all("meaRtools")
 
+times = system.file("extdata/textreader/demas.times", package="meaRtools")
+pos = system.file("extdata/textreader/demas.pos", package="meaRtools")
 
-source("~/langs/R/sjemea/R/corr_index.R")
-
-
-
-s = read_spikelist_text("~/proj/2018/maxwell/test1/wong1993_p0.times",
-                        "~/proj/2018/maxwell/test1/wong1993_p0.pos")
+s = read_spikelist_text(times, pos)
 
 meaRtools:::.plot_spike_list(s)
-meaRtools:::.plot_mealayout(s$layout, use_names = T, cex = 1)
+## following doesn't yet work
+##meaRtools:::.plot_mealayout(s$layout, use_names = T, cex = 1)
 
 
-
-breaks = seq(from=0, to=1000, by=50)
-s$corr = corr.index(s, distance.breaks = breaks)
-plot.corr.index(s, log="y", las=1, ylim=c(0.1, 100),
-                xlabel = ci.xlabel,
-                pch=20, show.fit=FALSE)
-
-
-
-
-
-
-
+##  breaks = seq(from=0, to=1000, by=50)
+##  s$corr = corr.index(s, distance.breaks = breaks)
+##  plot.corr.index(s, log="y", las=1, ylim=c(0.1, 100),
+##                xlabel = ci.xlabel, pch=20, show.fit=FALSE)
 
 ######################################################################
 
-## An example of 500 sec of data.
-s = read_spikelist_text("~/proj/2018/maxwell/test1/demas.times",
-                        "~/proj/2018/maxwell/test1/demas.pos")
-
-meaRtools:::.plot_spike_list(s)
-meaRtools:::.plot_mealayout(s$layout, use_names = T, cex = 1)
