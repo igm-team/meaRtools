525f89e05bab23b6080712ccb0ba0292007b48ae
Stephen J. Eglen <sje30@cam.ac.uk>
2018-06-22 11:34:46 +0100

    New test file for running multi-well plate

---
+++ meaRtools/inst/demas-test.R
@@ -0,0 +1,44 @@
+## Check the Demas well arrangement
+
+require(devtools)
+require(lattice)
+load_all()
+
+
+
+test_platelayout = list(n_well = 16,
+                        wells = paste0("w", "1:6"),
+                        n_well_r = 2,
+                        n_well_c = 3,
+                        layout = c(3, 2),
+                        n_elec_r = 8,
+                        n_elec_c = 8,
+                        xlim = c(-100, 7200),
+                        ylim = c(0, 6000),
+                        spacing = 200,
+                        corr_breaks = 0
+                        )
+
+add_plateinfo("test-6well", test_platelayout)
+times = system.file("extdata/textreader/demas.times", package="meaRtools")
+pos = system.file("extdata/textreader/demas.pos", package="meaRtools")
+s = read_spikelist_text(times, pos, array="test-6well")
+.plot_mealayout(s$layout, use_names = TRUE, cex=0.3)
+
+
+## This takes the average across the whole plate
+.plot_meanfiringrate(s, main = "Mean Firing Rate by Plate (Hz)")
+
+## TODO -- fix this up.
+##.channel_plot_by_well(s, resp = "meanfiringrate",
+##                      resp_label = "Mean Firing Rate (Hz)")
+
+s <- calculate_isis(s)
+.plot_isis_by_plate(s)
+
+## .plot_isis_by_electrode(s) ## does nothing?
+
+
+
+s = get_num_ae(s)
+.plot_mean_firingrate_by_electrode(s)
