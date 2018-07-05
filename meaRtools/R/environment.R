32b399d1dbcc791d4a11a25a42332986c56258fa
Stephen J. Eglen <sje30@cam.ac.uk>
2018-07-05 12:56:10 +0100

    Document environment.R functions

--- meaRtools/R/environment.R
+++ meaRtools/R/environment.R
@@ -32,7 +32,15 @@
 plateinfo = .axion_plateinfo
 
 
-
+##' Return information about an MEA plate format
+##'
+##' Given a plate name, return a list of information.
+##' If the plate name is not recognised, an error is generated.
+##' 
+##' @title Return information about an MEA plate format
+##' @param arrayname Plate name 
+##' @return A list storing information about that plate.
+##' @author Stephen Eglen
 get_plateinfo <- function(arrayname) {
   ## Return useful information related to arrayname
   ##
@@ -47,6 +55,27 @@ get_plateinfo <- function(arrayname) {
 }
 
 
+##' Return information about an MEA plate format
+##'
+##' Given a plate name, return a list of information.
+##' If the plate name is not recognised, an error is generated.
+##' 
+##' @title Return information about an MEA plate format
+##' @param arrayname Plate name 
+##' @return A list storing information about that plate.
+##' @author Stephen Eglen
+
+##' Provide information about a new MEA plate format
+##'
+##' Store information about a new platename called arrayname.
+##' If arrayname already has been used, overwrite it with this
+##' new information.
+##' 
+##' @title Provide information about a new MEA plate format.
+##' @param arrayname Name of the plate
+##' @param info List of information to store about this plate
+##' @return Nothing.
+##' @author Stephen Eglen
 add_plateinfo <- function(arrayname, info) {
   ## If arrayname data are alreeady given, this new information
   ## overwrites it.
